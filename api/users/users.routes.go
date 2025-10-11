d8a1e55626993e25f332abad0d06677a5921f8e0
JoaoCunha50 <joaomanuelmachadodacunha@gmail.com>
2025-10-11 19:56:09 +0100

    fix user routing

--- api/users/users.routes.go
+++ api/users/users.routes.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func RegisterUserRoutes(r *gin.Engine, repo *UserRepository) {
 	handler := NewUserHandler(repo)
 
-	r.POST("/users", handler.CreateUser)
-	r.GET("/users/:id", handler.GetUser)
-	r.GET("/users/login", handler.LoginUser)
+	r.POST("/", handler.CreateUser)
+	r.GET("/:id", handler.GetUser)
+	r.GET("/login", handler.LoginUser)
 }
